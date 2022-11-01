75ac5141f4a9a1e89fca7bfae5bf6465f862852a
Alfredo Aguiar Arce <alfredoaguiararce@gmail.com>
2022-11-01 07:34:45 -0600

    Update challenge.js

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,5 @@
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest; 
-const API = 'https://api.escuelajs.co/api/v1/products';
+const API = 'https://api.escuelajs.co/api/v1';
 
 /**
  * It takes a URL and a callback function as arguments, and makes an HTTP request to the URL. When the
@@ -16,17 +16,27 @@ function fetchData(urlApi, callback) {
   xhttp.onreadystatechange = function (event) {
     if (xhttp.readyState === 4) {
       if(xhttp.status === 200) {
-        callback(null, JSON.parse(xhttp.resposeText));
+        callback(null, JSON.parse(xhttp.responseText));
       }
-    } else {
-      const error = new Error('Error' + urlApi);
-      return callback(error, null);
-    }
+      else {
+        const error = new Error('Error' + urlApi);
+        return callback(error, null);
+      }
+    } 
   }
   xhttp.send();
 }
 
-/* Calling the function fetchData with the API URL and a callback function. */
-fetchData(API, (a, b) => {
-    console.log(b);
+/* A callback hell. */
+fetchData(`${API}/products`, (error1, data1) => {
+    if(error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, (error2, data2) => {
+        if(error2) return console.error(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, (error3, data3) => {
+          if(error3) return console.error(error3);
+          console.log(data1[0]);
+          console.log(data2.title);
+          console.log(data3.name);
+        });
+    });
 });
